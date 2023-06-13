960b03b0db8bc1348000e7b4c3e71cdf5aabb19c
Shohei Kikuchi <killchance2256@gmail.com>
2023-06-13 23:15:15 +0900

    feat: Add Footer

---
+++ src/component/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+function Footer () {
+  return (
+    <>
+      <footer>
+        <p>&copy; 2023/06 Shopeco</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer;
