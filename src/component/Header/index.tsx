5eba3bf7b85d8962d187a77aa3c7b133d054cc8e
Shohei Kikuchi <killchance2256@gmail.com>
2023-07-12 19:59:14 +0900

    feat: style header

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import StyldHeader from "./index.styled";
+
 
 function Header () {
   return (
-    <>
+    <StyldHeader>
       <header>
-        <h1>スロシミュ</h1>
-        <p>ジャグラーのシミュレーションができます</p>
+        <div className="title">
+          <h1>スロシミュ</h1>
+          <p>ジャグラーのシミュレーションができます</p>
+        </div>
+        <div className="menu">
+          <ul>
+            <li>
+              <a href="">開発者について</a>
+            </li>
+            <li>
+              <a href="">詳細</a>
+            </li>
+            <li>
+              <a href="">お問合せ</a>
+            </li>
+          </ul>
+        </div>
       </header>
-    </>
+    </StyldHeader>
   )
 }
 
