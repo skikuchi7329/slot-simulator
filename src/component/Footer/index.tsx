19201acbe4823c2d6c93d996fb845ba59d37841f
Shohei Kikuchi <killchance2256@gmail.com>
2023-07-12 21:04:57 +0900

    feat: style footer

--- src/component/Footer/index.tsx
+++ src/component/Footer/index.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-
+import StyledFooter from "./index.styled";
 
 
 function Footer () {
   return (
-    <>
+    <StyledFooter>
       <footer>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <a href="">開発者について</a>
+              </li>
+              <li>
+                <a href="">詳細</a>
+              </li>
+              <li>
+                <a href="">お知らせ</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <p>&copy; 2023/06 Shopeco</p>
       </footer>
-    </>
+    </StyledFooter>
   )
 }
 
