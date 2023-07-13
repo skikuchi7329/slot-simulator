be168bf380aabca2233ee64e2e540fac7beb6fac
Shohei Kikuchi <killchance2256@gmail.com>
2023-07-13 13:18:23 +0900

    feat: Style graphs, buttonand table

---
+++ src/component/Graphs/index.styled.tsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+const StyledGraphs = styled.div`
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px; /* 各要素の間隔を指定 */
+  }
+
+  .styled-button {
+    display: inline-block;
+    font-size: 1em;
+    padding: 10px 20px;
+    margin: 10px;
+    color: #ffffff;
+    background: linear-gradient(to right, #726a95, #2c88d9);
+    border: none;
+    border-radius: 5px;
+    text-decoration: none;
+    transition: all 0.3s ease-out;
+  }
+
+  .styled-button:hover {
+    background: linear-gradient(to right, #2c88d9, #726a95);
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+      0 17px 50px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  .styled-button:active {
+    background: #2c88d9;
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
+
+  .styled-table {
+    font-family: Arial, sans-serif;
+    border-collapse: collapse;
+    width: 60%;
+    margin: auto;
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.15);
+    color: #333;
+  }
+
+  .styled-table caption {
+    font-size: 1.5em;
+    margin-bottom: 10px;
+    text-align: left;
+  }
+
+  .styled-table th {
+    width: 20%; /* 幅を20%に変更 */
+    background-color: #333;
+    color: white;
+    padding: 10px;
+    text-align: left;
+  }
+
+  .styled-table td {
+    border: 1px solid #999;
+    padding: 10px;
+    text-align: center;
+  }
+
+  .styled-table tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+
+  .styled-table:last-of-type {
+    margin-bottom: 30px;
+  }
+`;
+
+export default StyledGraphs;
--- src/component/Graphs/index.tsx
+++ src/component/Graphs/index.tsx
@@ -1,13 +1,19 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import { Line } from "react-chartjs-2";
-import { Chart, LineController, LinearScale, PointElement, LineElement } from 'chart.js';
+import {
+  Chart,
+  LineController,
+  LinearScale,
+  PointElement,
+  LineElement,
+} from "chart.js";
+import StyledGraphs from "./index.styled";
 
 Chart.register(LineController, LinearScale, PointElement, LineElement);
 
-
-function Graphs () {
+function Graphs() {
   const [totalCoins, setTotalCoins] = useState(0);
-  const [results, setResults] = useState<{x: number; y: number;}[]>([]);
+  const [results, setResults] = useState<{ x: number; y: number }[]>([]);
   const [bbCount, setBBCount] = useState(0);
   const [rbCount, setRBCount] = useState(0);
   const [cherryCount, setCherryCount] = useState(0);
@@ -19,51 +25,55 @@ function Graphs () {
     const g = gcd(numerator, denominator);
     numerator = numerator / g;
     denominator = denominator / g;
-  
+
     if (numerator === 1) {
       return `${numerator}/${denominator.toFixed(2)}`;
     } else {
       return `1/${(denominator / numerator).toFixed(2)}`;
     }
   }
-  
+
   function gcd(a: number, b: number): number {
     if (!b) {
       return a;
     }
     return gcd(b, a % b);
   }
-  
 
   const random = () => {
     setTotalCoins(0);
-    setResults([{x: 0, y: 0}]); //初期値をセット
+    setResults([{ x: 0, y: 0 }]); //初期値をセット
     setTotalCoins((currentCoins) => {
-      let bb = 0, rb = 0, cherry = 0, replay = 0, grape = 0, miss = 0;
+      let bb = 0,
+        rb = 0,
+        cherry = 0,
+        replay = 0,
+        grape = 0,
+        miss = 0;
       let newCoins = currentCoins;
       let newResults = [];
-      for(let i = 0; i < 8000; i++) {
+      for (let i = 0; i < 8000; i++) {
         newCoins -= 3;
         const rondomNum = Math.floor(Math.random() * 65536 + 1);
         if (1 <= rondomNum && 255 >= rondomNum) {
           newCoins += 252; //BB
           bb++;
-        } else if ( 256 <= rondomNum && 511 >= rondomNum ){
+        } else if (256 <= rondomNum && 511 >= rondomNum) {
           newCoins += 96; //RB
           rb++;
-        } else if ( 512 <= rondomNum && 2592 >= rondomNum){
+        } else if (512 <= rondomNum && 2592 >= rondomNum) {
           newCoins += 2; //チェリー
           cherry++;
-        } else if ( 2593 <= rondomNum && 11571 >= rondomNum){
+        } else if (2593 <= rondomNum && 11571 >= rondomNum) {
           newCoins += 3; //リプレイ
           replay++;
-        } else if ( 11572 <= rondomNum && 22871 >= rondomNum ) {
+        } else if (11572 <= rondomNum && 22871 >= rondomNum) {
           newCoins += 8; //ぶどう
           grape++;
         } else {
           miss++;
         }
-        newResults.push({x: i, y: newCoins});
+        newResults.push({ x: i, y: newCoins });
       }
       setResults(newResults);
       setBBCount(bb);
@@ -74,16 +84,16 @@ function Graphs () {
       setMissCount(miss);
       return newCoins;
     });
-  }
+  };
 
   const data = {
     datasets: [
       {
-        label: 'Coin Trend',
+        label: "Coin Trend",
         data: results,
         fill: false,
-        backgroundColor: 'rgb(75, 192, 192)',
-        borderColor: 'rgba(75, 192, 192, 0.2)',
+        backgroundColor: "rgb(75, 192, 192)",
+        borderColor: "rgba(75, 192, 192, 0.2)",
       },
     ],
   };
@@ -91,78 +101,100 @@ function Graphs () {
   const options = {
     scales: {
       x: {
-        type: 'linear' as const,
-        beginAtZero: true
+        type: "linear" as const,
+        beginAtZero: true,
       },
       y: {
-        type: 'linear' as const,
-        beginAtZero: true
-      }
-    }
+        type: "linear" as const,
+        beginAtZero: true,
+      },
+    },
   };
 
-  const totalCount = bbCount + rbCount + cherryCount + replayCount + grapeCount + missCount;
-  
+  const totalCount =
+    bbCount + rbCount + cherryCount + replayCount + grapeCount + missCount;
+
   return (
-    <>
-      <button onClick={random}>スタート</button>
-      <Line data={data} options={options} />
-      <table>
-      <thead>
-        <tr>
-          <th>結果</th>
-          <th>回数</th>
-          <th>確率</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>収支</td>
-          <td>{totalCoins}</td>
-        </tr>
-        <tr>
-          <td>BB</td>
-          <td>{bbCount}</td>
-          <td>{fraction(bbCount, totalCount)}</td>
-        </tr>
-        <tr>
-          <td>RB</td>
-          <td>{rbCount}</td>
-          <td>{fraction(rbCount, totalCount)}</td>
-        </tr>
-        <tr>
-          <td>合算</td>
-          <td>{rbCount + bbCount}</td>
-          <td>{fraction(rbCount + bbCount, totalCount)}</td>
-        </tr>
-        <tr>
-          <td>チェリー</td>
-          <td>{cherryCount}</td>
-          <td>{fraction(cherryCount, totalCount)}</td>
-        </tr>
-        <tr>
-          <td>リプレイ</td>
-          <td>{replayCount}</td>
-          <td>{fraction(replayCount, totalCount)}</td>
-        </tr>
-        <tr>
-          <td>ぶどう</td>
-          <td>{grapeCount}</td>
-          <td>{fraction(grapeCount, totalCount)}</td>
-        </tr>
-        <tr>
-          <td>ハズレ</td>
-          <td>{missCount}</td>
-          <td>{fraction(missCount, totalCount)}</td>
-        </tr>
-        <tr>
-          <td>機械割</td>
-          <td>{(((totalCoins + 8000 * 3) / (8000 * 3) * 100)).toFixed(2)}%</td>
-        </tr>
-      </tbody>
-    </table>
-    </>
-  )
+    <StyledGraphs>
+      <div className="container">
+        <button className="styled-button" onClick={random}>
+          スタート
+        </button>
+
+        <div className="graph">
+          <Line data={data} options={options} />
+        </div>
+
+        <table className="styled-table">
+          <caption>結果</caption>
+          <tbody>
+            <tr>
+              <th>差枚数</th>
+              <td>{totalCoins}</td>
+            </tr>
+            <tr>
+              <th>収支</th>
+              <td>{totalCoins * 20}円</td>
+            </tr>
+            <tr>
+              <th>機械割</th>
+              <td>
+                {(((totalCoins + 8000 * 3) / (8000 * 3)) * 100).toFixed(2)}%
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table className="styled-table">
+          <caption>ボーナス・小役</caption>
+          <thead>
+            <tr>
+              <th></th>
+              <td>回数</td>
+              <td>確率</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>BB</th>
+              <td>{bbCount}</td>
+              <td>{fraction(bbCount, totalCount)}</td>
+            </tr>
+            <tr>
+              <th>RB</th>
+              <td>{rbCount}</td>
+              <td>{fraction(rbCount, totalCount)}</td>
+            </tr>
+            <tr>
+              <th>合算</th>
+              <td>{rbCount + bbCount}</td>
+              <td>{fraction(rbCount + bbCount, totalCount)}</td>
+            </tr>
+            <tr>
+              <th>チェリー</th>
+              <td>{cherryCount}</td>
+              <td>{fraction(cherryCount, totalCount)}</td>
+            </tr>
+            <tr>
+              <th>リプレイ</th>
+              <td>{replayCount}</td>
+              <td>{fraction(replayCount, totalCount)}</td>
+            </tr>
+            <tr>
+              <th>ぶどう</th>
+              <td>{grapeCount}</td>
+              <td>{fraction(grapeCount, totalCount)}</td>
+            </tr>
+            <tr>
+              <th>ハズレ</th>
+              <td>{missCount}</td>
+              <td>{fraction(missCount, totalCount)}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </StyledGraphs>
+  );
 }
 
 export default Graphs;
