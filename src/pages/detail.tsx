c80bfc96669c5f6b7d57eceecf4fab7f0d2cd5bf
Shohei Kikuchi <killchance2256@gmail.com>
2024-05-13 23:31:29 +0900

    chore

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import Header from "../component/Header/index"
 
 function Detail() {
   return (
-  <div></div>
-)}
+    <StyledDetail>
+      <div className="wrapper">
+        <Header />
+        <div className="header">
+          <h2>条件</h2>
+        </div>
+      </div>
+    </StyledDetail>
+  );
+}
 
+const StyledDetail = styled.div`
+  .wrapper {
+    margin: 0;
+    width: 100%;
+    height: 100%;
+  }
 
+  .header {
+    margin: 0 auto;
+    text-align: center:
+  }
+`
 
 export default Detail;
-
